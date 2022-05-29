6329b4d263d57e203edc308a4aafad1c034269a6
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2022-05-30 11:49:58 +1200

    Slider and Scrolling
    Added a Portfolio section, with a slider.
    Added titles for Sections.
    Added scrolling to About Me.

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const submitButtonjs = document.querySelector('#submitButton');
 
 function submitClick() {
     submitButtonjs.value = "Thank You!"
-    console.log("Thank you!")
+    console.log("SUBMIT button pressed")
 }
 
-// Clicking in Contact (navbar), takes you to the contact form
 submitButtonjs.addEventListener('click', submitClick);
+
+// Clicking in Contact (navbar), takes you to the contact form
 
 const contactButtonjs = document.getElementById('contactButton');
 const contactFormTitle = document.getElementById('contact');
@@ -18,9 +19,60 @@ function scrollToContact() {
         block: 'start',
         inline: 'start'
     });
-    console.log("Scrolling to contact")
+    console.log("Scrolled to contact form")
 }
 
 contactButtonjs.addEventListener('click', scrollToContact);
 
+// Clicking in ABOUT (navbar), takes you to the ABOUT section
 
+const aboutButtonjs = document.getElementById('aboutButton');
+const aboutHeader = document.getElementById('headerAboutMe');
+
+function scrollToAbout() {
+    aboutHeader.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+    console.log("Scrolled to about section")
+}
+
+aboutButtonjs.addEventListener('click', scrollToAbout);
+
+// Clicking in PORTFOLIO (navbar), takes you to the PORTFOLIO Slider
+
+const portfolioButtonjs = document.getElementById('portfolioButton');
+const portfolioHeader = document.getElementById('headerPortfolio');
+
+function scrollToPortfolio() {
+    portfolioHeader.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'start'
+    });
+    console.log("Scrolled to portfolio slider")
+}
+
+portfolioButtonjs.addEventListener('click', scrollToPortfolio);
+
+// Slider animation
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n)
+};
+
+function showDivs(n) {
+  var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+   
+}
