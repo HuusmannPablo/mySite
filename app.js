d065b1652e14dee6251efcce51836742e93eb166
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2022-06-01 11:46:17 +1200

    Sticky Navbar, Media Queries
    Added the sticky navbar function.
    Margins corrected for better visual.
    Media Queries modified for better visual.

--- app.js
+++ app.js
@@ -82,3 +82,22 @@ function showDivs(n) {
 // Show the picture with the right index (index in the array goes from 0 to n)
     mySlidesJS[slideIndex-1].style.display = "block"; 
 };
+
+// STICKY NAVBAR
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyFunction()};
+
+let navbar = document.getElementById("navbar");
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  };
+};
+
+
